36d25a96cb7edd4201d1a893e39d4ad3c282aef1
Victoria B <viktorija.bourassa@gmail.com>
2019-01-30 15:03:50 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,3 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
+
+extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
